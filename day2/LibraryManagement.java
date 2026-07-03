fc2da2bcf88d1dd7b885a2bea5f813eadeaec2b0
sruthichuie <sruthi2094@gmail.com>
2026-07-03 17:32:49 +0530

    Delete day2/LibraryManagement.java

--- day2/LibraryManagement.java
+++
@@ -1,9 +0,0 @@
-package org.testleaf.week1.day2;
-
-public class LibraryManagement {
-public static void main(String[] args) {
-    Library l= new Library();
-    System.out.println(l.addBook("The wings of fire"));
-    l.issueBook();
-}
-}
