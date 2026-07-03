87749d984b446e370956c2a4ba1ce2421321eb96
sruthichuie <sruthi2094@gmail.com>
2026-07-03 17:53:10 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,17 @@
+package org.testleaf.week1.day1;
+
+public class Chrome {
+    public static void main(String[] args) {
+        float version = 91.0f;
+        String developer= "Google";
+        boolean isBeta = false;
+        int releaseyear = 2008;
+        char shortcutkey = 'C';
+        System.out.println("\nchrome version: " +version+ 
+        "\ndeveloped by: "+developer+ "\nits beta version: " +isBeta+
+        "\nrelease year: "+releaseyear+ "\nand shortcutkey is: " +shortcutkey
+        );
+
+    }
+
+}
