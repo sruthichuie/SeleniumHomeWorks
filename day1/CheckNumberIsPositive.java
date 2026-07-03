d6b855ac613ac10b84905efdd002d7eca9ad40f2
sruthichuie <sruthi2094@gmail.com>
2026-07-03 15:35:26 +0530

    Add files via upload

---
+++ day1/CheckNumberIsPositive.java
@@ -0,0 +1,13 @@
+package org.testleaf.week1.day1;
+
+public class CheckNumberIsPositive {
+    public static void main(String[] args) {
+      int num =10;
+      if(num<0)
+        System.out.println("The number is negative");
+      if(num>0)
+        System.out.println("The number is positive");
+  
+    }
+
+}
